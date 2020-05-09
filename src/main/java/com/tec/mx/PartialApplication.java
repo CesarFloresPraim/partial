37da29b1dd3c3a503babf7e83499c8173348ac2d
Cesar <cesar_florespal@outlook.com>
2020-05-08 18:07:45 -0600

    first commit

---
+++ src/main/java/com/tec/mx/PartialApplication.java
@@ -0,0 +1,13 @@
+package com.tec.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tec/mx/PartialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
