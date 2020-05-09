545928082ae22cc1083a7949d56da6606b0f0b48
Cesar <cesar_florespal@outlook.com>
2020-05-08 19:32:40 -0600

    question 3

--- src/main/java/com/tec/mx/rest/EventRestController.java
+++ src/main/java/com/tec/mx/rest/EventRestController.java
@@ -1,14 +1,17 @@
 package com.tec.mx.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import javax.validation.constraints.PositiveOrZero;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,17 @@ public class EventRestController {
 	    Event eventInfo = eventsRepository.save(newEvent);
 		return new ResponseEntity<Object>(eventInfo, HttpStatus.OK);
 	}
+	
+	@GetMapping("/api/events/{id}")
+	public ResponseEntity<Object> getEventById(@PathVariable(value = "id") 
+							@PositiveOrZero(message = "Id must be greater or equal to 0") Long id) {
+		if (id <= 0) {
+	        return new ResponseEntity<Object>("Id must be greater or equal to 0", HttpStatus.NOT_FOUND);
+		}
+		Optional<Event> eventFound = eventsRepository.findById(id);
+		
+	    return new ResponseEntity<Object>(eventFound, HttpStatus.OK);
+		
+	}	
 	
 }
